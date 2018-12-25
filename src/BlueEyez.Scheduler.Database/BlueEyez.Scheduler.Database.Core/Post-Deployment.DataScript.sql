df9cbd4c34e969d41235ee105101cd834500dd9e
MichaelGrabchak <MichaelGrabchak@users.noreply.github.com>
2018-12-25 19:07:15 +0200

    Delete Post-Deployment.DataScript.sql

--- src/BlueEyez.Scheduler.Database/BlueEyez.Scheduler.Database.Core/Post-Deployment.DataScript.sql
+++
@@ -1,4 +0,0 @@
-:r E:\Development\Sources\BlueEyez.Scheduler.Database\src\BlueEyez.Scheduler.Database\BlueEyez.Scheduler.Database.Core\Post-Deployment\dbo\Tables\EngineStatus.sql
-:r E:\Development\Sources\BlueEyez.Scheduler.Database\src\BlueEyez.Scheduler.Database\BlueEyez.Scheduler.Database.Core\Post-Deployment\dbo\Tables\Instance.Data.sql
-:r E:\Development\Sources\BlueEyez.Scheduler.Database\src\BlueEyez.Scheduler.Database\BlueEyez.Scheduler.Database.Core\Post-Deployment\dbo\Tables\JobStatus.Data.sql
-:r E:\Development\Sources\BlueEyez.Scheduler.Database\src\BlueEyez.Scheduler.Database\BlueEyez.Scheduler.Database.Core\Post-Deployment\dbo\Tables\Setting.Data.sql
